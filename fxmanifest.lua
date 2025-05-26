935f2284ca1f9743d79e37b409c2a4c7419fcd43
HXVRMXN-dev <107282158+HXVRMXN-dev@users.noreply.github.com>
2025-05-26 23:20:18 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'HXVRMXN.dev & IcePickPlays'
+description 'Weapon Animation Overhaul V2'
+version '1.0.0'
+
+client_scripts {
+    'WAOverride.lua'
+}
+
+files {
+    'cnt/*.meta'
+}
+
+data_file 'WEAPON_ANIMATIONS_FILE' 'cnt/*.meta'
+
+
